1ff560ba660f9996fc372c18fb4564ffb93d9291
Jonathan Coates <coatesjonny01@gmail.com>
2026-09-17 22:51:29 +0100

    Fix pullquote

--- resources/js/sections/PullQuote.vue
+++ resources/js/sections/PullQuote.vue
@@ -5,9 +5,9 @@ defineProps({ quote: { type: String, default: '' } })
 </script>
 
 <template>
-    <RevealSection class="mx-auto max-w-[720px] px-[24px] pt-[80px] text-center">
+    <RevealSection class="mx-auto max-w-180 px-6 pt-20 text-center">
         <div
-            class="rich m-0 font-serif text-[clamp(24px,3vw,34px)] leading-[1.4] text-ink text-pretty"
+            class="rich m-0 [&>strong]:font-serif [&>strong]:text-[clamp(24px,3vw,34px)] leading-[1.4] text-ink text-pretty"
             v-html="quote"
         ></div>
     </RevealSection>
